0bb07498f1a6ea19c2e8cab15a958953804bed4d
SpinBoxx <quentin.mimault@orange.fr>
2023-08-08 20:34:16 +0200

    [STYLE] Added px-4 to homepage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -14,7 +14,7 @@ const HomePage = async () => {
     <Container>
       <div className="space-y-8 pb-10">
         <Billboard data={billboard} />
-        <div className="flex flex-col gap-y-8 sm:px-6 lg:px-8">
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured products" products={products} />
         </div>
       </div>
